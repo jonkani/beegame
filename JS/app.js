0e7293432aa94543fe528d6cf089d6d3fe31bc2e
John Carney <jcarney000@gmail.com>
2016-06-02 14:59:23 -0700

    add main menu and materialize

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,11 @@
 var gameProperties = {
-    screenWidth: 1000,
-    screenHeight: 650,
+    screenWidth: 900,
+    screenHeight: 600,
 };
 
 var states = {
     game: "game",
+    menu: "menu"
 };
 
 var assets = {
@@ -47,11 +48,14 @@ gameState.prototype = {
       game.load.image(assets.bee2.name, assets.bee2.URL);
       game.load.image(assets.stinger1.name, assets.stinger1.URL);
       game.load.image(assets.stinger2.name, assets.stinger2.URL);
-      this.game.load.physics("stinger_physics", "assets/stinger2.json");
+      this.game.load.physics("stinger_physics", "Sprites/Stinger2.json");
     },
 
     create: function () {
-      game.world.setBounds(0, 0, 1000, 650);
+      game.world.setBounds(0, 0, 900, 600);
+      this.game.scale.pageAlignHorizontally = true;
+      this.game.scale.pageAlignVertically = true;
+      this.game.scale.refresh();
       game.physics.startSystem(Phaser.Physics.P2JS);
       game.physics.p2.setImpactEvents(true);
       game.physics.p2.restitution = 0.8;
@@ -75,8 +79,14 @@ gameState.prototype = {
       this.beeSprite2.body.angle = 180;
       this.game.physics.p2.enable(this.stinger1);
       this.game.physics.p2.enable(this.stinger2);
-      this.stinger1.body.kinematic = true;
-      this.stinger2.body.kinematic = true;
+
+      this.stinger1.body.clearShapes();
+      this.stinger1.body.loadPolygon("stinger_physics", 'Stinger1');
+      this.stinger2.body.clearShapes();
+      this.stinger2.body.loadPolygon("stinger_physics", 'Stinger2');
+
+      // this.stinger1.body.kinematic = true;
+      // this.stinger2.body.kinematic = true;
 
 
       this.stingPositioner1 = game.add.sprite(0, 70);
@@ -135,28 +145,36 @@ gameState.prototype = {
       this.bee1StingerGroup = this.game.physics.p2.createCollisionGroup();
       this.bee2StingerGroup = this.game.physics.p2.createCollisionGroup();
       this.terrainGroup = this.game.physics.p2.createCollisionGroup();
+
       // assign block collsion groups
       this.blockT.body.setCollisionGroup(this.terrainGroup);
       this.blockB.body.setCollisionGroup(this.terrainGroup);
       this.blockL.body.setCollisionGroup(this.terrainGroup);
       this.blockR.body.setCollisionGroup(this.terrainGroup);
+
       // assign bee collision groups
       this.beeSprite1.body.setCollisionGroup(this.bee1BodyGroup);
       this.stinger1.body.setCollisionGroup(this.bee1StingerGroup);
       this.beeSprite2.body.setCollisionGroup(this.bee2BodyGroup);
       this.stinger2.body.setCollisionGroup(this.bee2StingerGroup);
+
       // assign physiscs collisions
       this.blockT.body.collides([this.bee1BodyGroup, this.bee2BodyGroup]);
       this.blockB.body.collides([this.bee1BodyGroup, this.bee2BodyGroup]);
       this.blockL.body.collides([this.bee1BodyGroup, this.bee2BodyGroup]);
       this.blockR.body.collides([this.bee1BodyGroup, this.bee2BodyGroup]);
-      this.beeSprite1.body.collides([this.bee2BodyGroup, this.terrainGroup]);
-      this.beeSprite2.body.collides([this.bee1BodyGroup, this.terrainGroup]);
+      this.beeSprite1.body.collides([this.bee2BodyGroup, this.bee2StingerGroup, this.terrainGroup]);
+      this.beeSprite2.body.collides([this.bee1BodyGroup, this.bee2StingerGroup,  this.terrainGroup]);
 
+      // assign stinger collisions
+      this.stinger1.body.collides([this.bee2StingerGroup, this.bee2BodyGroup]);
+      this.stinger2.body.collides([this.bee1StingerGroup, this.bee2BodyGroup]);
 
-      // stinger collision
-      // this.beeSprite1.body.createBodyCallback(this.beeSprite2.children[0].body, this.player1Stung, this);
-      // this.beeSprite2.children[0].body.createBodyCallback(this.beeSprite1, this.player1Stung, this);
+      // this.stinger1.body.collides(this.bee2StingerGroup, this.stingerRepulse, this);
+      // this.stinger2.body.collides(this.bee1StingerGroup, this.stingerRepulse, this);
+
+      this.stinger1.body.createBodyCallback(this.stinger2, this.stingerRepulse, this);
+      this.stinger2.body.createBodyCallback(this.stinger1, this.stingerRepulse, this);
 
       this.initKeyboard();
       game.stage.backgroundColor = '#ffff00';
@@ -184,8 +202,43 @@ gameState.prototype = {
       this.key_forward2 = game.input.keyboard.addKey(Phaser.Keyboard.UP);
     },
 
-    player1Stung: function(body1, body2) {
-      console.log('1stung');
+    stingerRepulse: function(body1, body2) {
+      // var bee1X = this.beeSprite1.body.velocity.x;
+      // var bee1Y = this.beeSprite1.body.velocity.y;
+      // var bee2X = this.beeSprite2.body.velocity.x;
+      // var bee2Y = this.beeSprite2.body.velocity.y;
+      // console.log("before x:"+this.beeSprite1.body.velocity.x);
+      // console.log("before y:"+this.beeSprite1.body.velocity.y);
+      //
+      // if (Math.sign(this.beeSprite1.body.velocity.x) === 1) {
+      //   this.beeSprite1.body.velocity.x = -this.beeSprite1.body.velocity.x -200;
+      // }
+      // else if (Math.sign(this.beeSprite1.body.velocity.x) === -1) {
+      //   this.beeSprite1.body.velocity.x = -this.beeSprite1.body.velocity.x +200;
+      // }
+      //
+      // if (Math.sign(this.beeSprite1.body.velocity.y) === 1) {
+      //   this.beeSprite1.body.velocity.x = -this.beeSprite1.body.velocity.y -200;
+      // }
+      // else if (Math.sign(this.beeSprite1.body.velocity.y) === -1) {
+      //   this.beeSprite1.body.velocity.x = -this.beeSprite1.body.velocity.y +200;
+      // }
+      //
+      // if (Math.sign(this.beeSprite2.body.velocity.x) === 1) {
+      //   this.beeSprite2.body.velocity.x = -this.beeSprite2.body.velocity.x -200;
+      // }
+      // else if (Math.sign(this.beeSprite1.body.velocity.x) === -1) {
+      //   this.beeSprite2.body.velocity.x = -this.beeSprite2.body.velocity.x +200;
+      // }
+      //
+      // if (Math.sign(this.beeSprite1.body.velocity.y) === 1) {
+      //   this.beeSprite2.body.velocity.x = -this.beeSprite2.body.velocity.y -200;
+      // }
+      // else if (Math.sign(this.beeSprite1.body.velocity.y) === -1) {
+      //   this.beeSprite2.body.velocity.x = -this.beeSprite2.body.velocity.y +200;
+      // }
+      // console.log("after x:"+this.beeSprite1.body.velocity.x);
+      // console.log("after y:"+this.beeSprite1.body.velocity.y);
     },
 
 
@@ -228,5 +281,10 @@ gameState.prototype = {
 };
 
 var game = new Phaser.Game(gameProperties.screenWidth, gameProperties.screenHeight, Phaser.AUTO, 'gameDiv');
+var bee1Name = "";
+var bee2Name = "";
+var bee1Adj = "";
+var bee2Adj = "";
 game.state.add(states.game, gameState);
-game.state.start(states.game);
+game.state.add(states.menu, Menu)
+game.state.start(states.menu);
---
+++ JS/menu.js
@@ -0,0 +1,84 @@
+var Menu = {
+
+    preload : function() {
+        game.load.image('menuBee', './sprites/bee.png');
+        game.load.image('questionMark', './sprites/questionmark.png');
+    },
+
+    create: function () {
+        this.game.scale.pageAlignHorizontally = true;
+        this.game.scale.pageAlignVertically = true;
+        this.game.scale.refresh();
+        game.stage.backgroundColor = '#ffff00';
+
+        var hBlockShape = this.game.add.bitmapData(this.game.world.width, 10);
+        var vBlockShape = this.game.add.bitmapData(10, this.game.world.height);
+
+        // fill block shapes
+        hBlockShape.ctx.rect(0, 0, this.game.world.width, 10);
+        hBlockShape.ctx.fillStyle = '000';
+        hBlockShape.ctx.fill();
+
+        vBlockShape.ctx.rect(0, 0, 10, this.game.world.height);
+        vBlockShape.ctx.fillStyle = '000';
+        vBlockShape.ctx.fill();
+
+        // create top wall
+        this.blockT = this.game.add.sprite(this.game.world.width/2, 5, hBlockShape);
+        this.blockT.anchor.setTo(0.5, 0.5);
+
+        // create bottom wall
+        this.blockB = this.game.add.sprite(this.game.world.width/2, this.game.world.height - 5, hBlockShape)
+        this.blockB.anchor.setTo(0.5, 0.5);
+
+        // create left wall
+        this.blockL = this.game.add.sprite(5, this.game.world.height/2, vBlockShape)
+        this.blockL.anchor.setTo(0.5, 0.5);
+
+        // create right wall
+        this.blockR = this.game.add.sprite(this.game.world.width - 5, this.game.world.height/2, vBlockShape)
+        this.blockR.anchor.setTo(0.5, 0.5);
+
+        // add bee button
+        menuBee = game.add.button(this.game.world.width/2, this.game.world.height/2, 'menuBee', this.startGame, this, 2, 1, 0);
+        menuBee.anchor.setTo(0.5, 0.5);
+
+        // add help button
+        menuHelp = game.add.button(this.game.world.width/2, this.game.world.height/1.2, 'questionMark', this.help, this, 2, 1, 0);
+        menuHelp.anchor.setTo(0.5, 0.5);
+    },
+
+    update: function() {
+      menuBee.angle += 3;
+    },
+
+    help: function() {
+       $('#helpModal').openModal();
+    },
+
+    startGame: function() {
+      var $xhr = $.getJSON('http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=false&includePartOfSpeech=proper-noun&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=3&maxLength=10&limit=2&api_key=78c54330c7deecd16a3010c08a3090514ad52320e617e2a93');
+
+      $xhr.done(function(data) {
+      if ($xhr.status !== 200) {
+          return;
+      }
+        bee1Name = data[0].word;
+        bee2Name = data[1].word;
+        var $xhr2 = $.getJSON('    http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=false&includePartOfSpeech=adjective&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=3&maxLength=10&limit=2&api_key=78c54330c7deecd16a3010c08a3090514ad52320e617e2a93');
+        $xhr2.fail(function(a, b, c) {
+          console.log(arguments);
+        });
+        $xhr2.done(function(data) {
+          if ($xhr2.status !== 200) {
+            return;
+          }
+          bee1Adj = data[0].word.charAt(0).toUpperCase() + data[0].word.slice(1);
+          bee2Adj = data[1].word.charAt(0).toUpperCase() + data[1].word.slice(1);
+        });
+        console.log(bee1Name+bee1Adj, bee2Adj+bee2Adj);
+        game.state.start(states.game);
+      });
+    }
+
+};
